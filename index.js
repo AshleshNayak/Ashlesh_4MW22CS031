4d7ec90666d6290cd195931b7acce9cccb2fe3df
Ashlesh Nayak <143422745+AshleshNayak@users.noreply.github.com>
2024-07-16 22:07:41 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,38 @@
+// 1.Write a JavaScript function named findMax that takes an array of numbers as input 
+// and returns the maximum number from the array. If the array is empty, the function should return null.
+
+// function findMax(numbers) {
+//     if (numbers.length === 0) {
+//         return null;
+//     }
+//     let max = numbers[0];
+//     for (let i = 1; i < numbers.length; i++) {
+//         if (numbers[i] > max) {
+//             max = numbers[i];
+//         }
+//     }
+//     return max;
+// }
+// console.log(findMax([3, 5, 7, 2, 8])); 
+// console.log(findMax([]));
+
+
+// 2.Write a simple server using Express that responds with "Hello, World!" when accessed through the browser at 
+// the route "/myapi". Share a screenshot of the server displaying the "Hello, World!" message.
+
+// const express = require('express');
+// const app = express();
+// const port = 5000;
+
+// app.get('/myapi', (req, res) => {
+//   res.send('Hello, World!');
+// });
+
+// app.listen(port, () => {
+//   console.log("Server is running at 5000")
+// })
+
+
+
+
+
